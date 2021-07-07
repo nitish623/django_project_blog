edf05fa6c8af07ef331286b6d7a5bebb01231893
Your Name <you@example.com>
2021-07-07 19:17:25 +0530

    blog app deg=bugged

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['mynitishblogapp.herokuapp.com']
-
 
 # Application definition
 
@@ -140,3 +139,5 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 django_heroku.settings(locals())
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.db import models
 
+class MyModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
